9df8f08bdb8c34b56e57a916e6c3c50f99a6a9e7
Lufei1010 <lufeiwu1010@gmail.com>
2024-11-12 18:39:47 +0200

    Updated RestaurantController and RestaurantRoute for Restaurant Detail Page functionality

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -1,19 +1,28 @@
-import express from 'express';
-import { param } from 'express-validator';
-import RestaurantController from '../controllers/RestaurantController';
+import express from "express";
+import { param } from "express-validator";
+import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
 
+router.get(
+  "/:restaurantId",
+  param("restaurantId")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("RestaurantId parament must be a valid string"),
+    RestaurantController.getRestaurant
+);
+
 // /api/restaurant/search/london
 router.get(
-    "/search/:city",
-     param("city")
-     .isString()
-     .trim()
-     .notEmpty()
-     .withMessage("City parament must be a valid string"),
-     RestaurantController.searchRestaurant
-     )
-
+  "/search/:city",
+  param("city")
+    .isString()
+    .trim()
+    .notEmpty()
+    .withMessage("City parament must be a valid string"),
+  RestaurantController.searchRestaurant
+);
 
 export default router;
